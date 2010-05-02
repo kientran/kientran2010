9658968e12bb02e58b3f9412ac53ce6b01ca5f87
Kien Tran <kientran@kientran.com>
2010-05-01 22:17:01 -0500

    Fix to make tab not add # sign at end of url on click

--- js/site.js
+++ js/site.js
@@ -1,19 +1,22 @@
 $(document).ready(function() {
     
     // Expand Panel
-    $("#open").click(function(){
+    $("#open").click(function(e){
+        e.preventDefault();
         $("div#panel").slideDown("slow");   
         //$("div#pulldown").css("background", "#031740");
     }); 
     
     // Collapse Panel
-    $("#close").click(function(){
+    $("#close").click(function(e){
+        e.preventDefault();
         $("div#panel").slideUp("slow"); 
         //$("div#pulldown").css("background", "#010d26");
     });     
     
     // Switch buttons from "Log In | Register" to "Close Panel" on click
-    $("#pulldown a").click(function () {
+    $("#pulldown a").click(function (e) {
+        e.preventDefault();
         $("#pulldown a").toggle();
     }); 
         
